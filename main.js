d2aa6b9de6c337759af8a758f6cad2a41d4a6c17
Caseywalker <casey.walker118@gmail.com>
2021-01-16 11:57:58 -0600

    worked with functions in main js

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('JS-Intro')
+// console.log('JS-Intro')
+
+// function sayHello() {
+//   console.log('Hello!')
+// };
+
+// const functionTwo = function() {
+//   console.log('Hello2')
+// };
+
+// const functionArrow = () => {
+//   console.log('Hello, Arrow!')
+// };
+
+// sayHello();
+// functionTwo();
+// functionArrow();
+
+// const withArg = function(name) {
+//   console.log(`Hello, ${name}!`)
+// }
+
+// withArg('Casey')
+
+const addsTwoNumbers = (num1, num2) => {
+  const sum = num1 + num2; 
+  console.log(sum);
+  return sum;
+};
+
+console.log(addsTwoNumbers(2, 8));
