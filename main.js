92091c7d63687868e20d07920316c4beb67e18a1
Caseywalker <casey.walker118@gmail.com>
2021-01-16 11:07:50 -0600

    completed variables, data, and math

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log('JS-Intro')
+// console.log('JS-Intro')
+// ****** Start Variables ***** 
+// let firstName = 'Beyonce';
+// let lastName = 'Knowles';
+
+// console.log(firstName + ' ' + lastName)
+// console.log(`${firstName} ${lastName}`)
+
+// firstName = 'Casey';
+// lastName = 'Walker';
+
+// console.log(firstName + ' ' + lastName)
+// console.log(`${firstName} ${lastName}`)
+
+// let favoriteFood = 'Pizza';
+// console.log(favoriteFood);
+
+// favoriteFood = 'Tacos';
+// console.log(favoriteFood)
+//  ****** End Variables *****
+
+// ***** Start Data Types ***** 
+// const dog = 'bark';
+// console.log(typeof dog);
+
+// const answer = 33;
+// const negative = -14; 
+// console.log(typeof answer, typeof negative)
+
+// const iLoveJavascript = true;
+// console.log(typeof iLoveJavascript);
+
+// ***** End Data Types *****
+
+// ***** Start Math *****
+// console.log(Math.pow(2, 2));
+// console.log(Math.pow(3, 2));
+// console.log(Math.pow(3, 3));
+
+// console.log(Math.round(6.5));
+// console.log(Math.round(6.45));
+// console.log(Math.floor(9.7));
+// console.log(Math.ceil(6.3));
+const cat = 'kitty';
+console.log(cat.length);
+
+
+// ***** End Math *****
